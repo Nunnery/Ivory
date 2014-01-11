a28e356687f233a075faa28d90dfb8ff3ce71102
Richard Harrah <topplethenunnery@gmail.com>
2014-01-11 18:00:01 -0500

    adding enchantments to toJSON method

--- ivory-standalone/src/main/java/net/nunnerycode/bukkit/libraries/ivory/utils/JSONUtils.java
+++ ivory-standalone/src/main/java/net/nunnerycode/bukkit/libraries/ivory/utils/JSONUtils.java
@@ -1,7 +1,10 @@
 package net.nunnerycode.bukkit.libraries.ivory.utils;
 
+import org.bukkit.enchantments.Enchantment;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class JSONUtils {
 
@@ -9,15 +12,24 @@ public class JSONUtils {
 		// do nothing
 	}
 
-	public static String toJSON(int id, byte data, String name, List<String> lore) {
-		return String.format("{id:%d,data:%s,tag:{display:{Name:%s,Lore:%s}}}", id, data, name,
-				quoteList(lore).toString());
+	public static String toJSON(int id, byte data, String name, List<String> lore, Map<Enchantment,
+			Integer> enchantments) {
+		return String.format("{id:%d,data:%s,tag:{display:{Name:%s,Lore:%s},ench:%s}}", id, data, name,
+				quoteList(lore).toString(), enchantmentMapToStringList(enchantments));
 	}
 
 	private static List<String> quoteList(List<String> lore) {
 		List<String> strings = new ArrayList<>();
 		for (String s : lore) {
 			strings.add("\"" + s.replace(":", "|") + "\"");
+		}
+		return strings;
+	}
+
+	private static List<String> enchantmentMapToStringList(Map<Enchantment, Integer> enchantmentIntegerMap) {
+		List<String> strings = new ArrayList<>();
+		for (Map.Entry<Enchantment, Integer> entry : enchantmentIntegerMap.entrySet()) {
+			strings.add("{id:" + entry.getKey().getId() + ",lvl:" + entry.getValue() + "}");
 		}
 		return strings;
 	}
--- ivory-standalone/src/test/java/net/nunnerycode/bukkit/libraries/ivory/utils/JSONUtilsTest.java
+++ ivory-standalone/src/test/java/net/nunnerycode/bukkit/libraries/ivory/utils/JSONUtilsTest.java
@@ -1,10 +1,13 @@
 package net.nunnerycode.bukkit.libraries.ivory.utils;
 
-import junit.framework.Assert;
+import org.bukkit.enchantments.Enchantment;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class JSONUtilsTest {
 	@Test
@@ -13,10 +16,14 @@ public class JSONUtilsTest {
 		byte data = 0;
 		String name = "TEST NAME PLEASE IGNORE";
 		List<String> list = Arrays.asList("Test Lore", "Please Ignore");
+		Map<Enchantment, Integer> enchantments = new LinkedHashMap<>();
+		enchantments.put(Enchantment.DAMAGE_ALL, 2);
+		enchantments.put(Enchantment.LOOT_BONUS_MOBS, 1);
+		enchantments.put(Enchantment.DAMAGE_UNDEAD, 4);
 
 		String expected = "{id:257,data:0,tag:{display:{Name:TEST NAME PLEASE IGNORE,Lore:[\"Test Lore\", " +
-				"\"Please Ignore\"]}}}";
-		String actual = JSONUtils.toJSON(id, data, name, list);
+				"\"Please Ignore\"]},ench:[{id:16,lvl:2}, {id:21,lvl:1}, {id:17,lvl:4}]}}";
+		String actual = JSONUtils.toJSON(id, data, name, list, enchantments);
 
 		Assert.assertEquals(expected, actual);
 	}
