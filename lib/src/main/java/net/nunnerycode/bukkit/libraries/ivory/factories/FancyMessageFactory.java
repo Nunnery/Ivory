ca54c4f234dc5bc64af8cb14a62e560beef23f11
Richard Harrah <rmh4209@gmail.com>
2014-02-02 02:03:04 -0500

    adding FancyMessageFactory

---
+++ lib/src/main/java/net/nunnerycode/bukkit/libraries/ivory/factories/FancyMessageFactory.java
@@ -0,0 +1,63 @@
+package net.nunnerycode.bukkit.libraries.ivory.factories;
+
+import org.bukkit.Bukkit;
+
+import mkremins.fanciful.IFancyMessage;
+
+public final class FancyMessageFactory {
+
+  private static FancyMessageFactory INSTANCE;
+
+  private FancyMessageFactory() {
+    // do nothing;
+  }
+
+  public static FancyMessageFactory getInstance() {
+    if (INSTANCE == null) {
+      INSTANCE = new FancyMessageFactory();
+    }
+    return INSTANCE;
+  }
+
+  /**
+   * Creates a new {@link mkremins.fanciful.IFancyMessage} for use.
+   * </br>
+   * Returns null if using a package that's not recognized.
+   * @return new IFancyMessage
+   */
+  public IFancyMessage getNewFancyMessage() {
+    String pkg = Bukkit.getServer().getClass().getPackage().getName();
+    if (pkg.equals("org.bukkit.craftbukkit.v1_5_R2")) {
+      return new mkremins.fanciful.v_1_5_R2.FancyMessage("");
+    }
+    if (pkg.equals("org.bukkit.craftbukkit.v1_6_R3")) {
+      return new mkremins.fanciful.v_1_6_R3.FancyMessage("");
+    }
+    if (pkg.equals("org.bukkit.craftbukkit.v1_6_R2")) {
+      return new mkremins.fanciful.v_1_6_R2.FancyMessage("");
+    }
+    if (pkg.equals("org.bukkit.craftbukkit.v1_6_R1")) {
+      return new mkremins.fanciful.v_1_6_R1.FancyMessage("");
+    }
+    if (pkg.equals("org.bukkit.craftbukkit.v1_5_R3")) {
+      return new mkremins.fanciful.v_1_5_R3.FancyMessage("");
+    }
+    if (pkg.equals("org.bukkit.craftbukkit.v1_5_R2")) {
+      return new mkremins.fanciful.v_1_5_R2.FancyMessage("");
+    }
+    if (pkg.equals("org.bukkit.craftbukkit.v1_5_R1")) {
+      return new mkremins.fanciful.v_1_5_R1.FancyMessage("");
+    }
+    if (pkg.equals("org.bukkit.craftbukkit.v1_4_R1")) {
+      return new mkremins.fanciful.v_1_4_R1.FancyMessage("");
+    }
+    if (pkg.equals("org.bukkit.craftbukkit.v1_4_6")) {
+      return new mkremins.fanciful.v_1_4_6.FancyMessage("");
+    }
+    if (pkg.equals("org.bukkit.craftbukkit.v1_4_5")) {
+      return new mkremins.fanciful.v_1_4_5.FancyMessage("");
+    }
+    return null;
+  }
+
+}
