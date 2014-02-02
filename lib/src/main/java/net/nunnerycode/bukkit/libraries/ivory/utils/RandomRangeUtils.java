c63b2f49cbb8c2f4133c0da70a6c2ddd2ff1dfda
Richard Harrah <rmh4209@gmail.com>
2014-02-02 01:45:22 -0500

    removing Commons Lang usage from RandomRangeUtils

--- lib/src/main/java/net/nunnerycode/bukkit/libraries/ivory/utils/RandomRangeUtils.java
+++ lib/src/main/java/net/nunnerycode/bukkit/libraries/ivory/utils/RandomRangeUtils.java
@@ -1,10 +1,10 @@
 package net.nunnerycode.bukkit.libraries.ivory.utils;
 
-import org.apache.commons.lang.math.DoubleRange;
-import org.apache.commons.lang.math.LongRange;
-import org.apache.commons.lang.math.RandomUtils;
+import java.util.Random;
 
 public final class RandomRangeUtils {
+
+  private static final Random RANDOM = new Random();
 
   private RandomRangeUtils() {
     // do nothing
@@ -20,7 +20,7 @@ public final class RandomRangeUtils {
   public static long randomRangeLongExclusive(long value1, long value2) {
     long max = Math.max(value1, value2);
     long min = Math.min(value1, value2);
-    long value = min + (long) (RandomUtils.nextDouble() * (max - min));
+    long value = min + (long) (RANDOM.nextDouble() * (max - min));
     return Math.min(Math.max(value, min), max - 1);
   }
 
@@ -34,33 +34,7 @@ public final class RandomRangeUtils {
   public static long randomRangeLongInclusive(long value1, long value2) {
     long max = Math.max(value1, value2);
     long min = Math.min(value1, value2);
-    long value = min + (long) (RandomUtils.nextDouble() * (max - min + 1));
-    return Math.min(Math.max(value, min), max);
-  }
-
-  /**
-   * Returns a value based on a LongRange, including its lower value but not the upper
-   *
-   * @param rangeContainer LongRange to check against
-   * @return a value based on a LongRange, including its lower value but not the upper
-   */
-  public static long randomLongFromLongRangeExclusive(LongRange rangeContainer) {
-    long min = rangeContainer.getMinimumLong();
-    long max = rangeContainer.getMaximumLong();
-    long value = min + (long) (RandomUtils.nextDouble() * (max - min));
-    return Math.min(Math.max(value, min), max - 1);
-  }
-
-  /**
-   * Returns a value based on a LongRange, including both its lower and upper value
-   *
-   * @param rangeContainer LongRange to check against
-   * @return value
-   */
-  public static long randomLongFromLongRangeInclusive(LongRange rangeContainer) {
-    long min = rangeContainer.getMinimumLong();
-    long max = rangeContainer.getMaximumLong();
-    long value = min + (long) (RandomUtils.nextDouble() * (max - min) + 1);
+    long value = min + (long) (RANDOM.nextDouble() * (max - min + 1));
     return Math.min(Math.max(value, min), max);
   }
 
@@ -74,7 +48,7 @@ public final class RandomRangeUtils {
   public static double randomRangeDoubleExclusive(double value1, double value2) {
     double min = Math.min(value1, value2);
     double max = Math.max(value1, value2);
-    double value = min + RandomUtils.nextDouble() * (max - min);
+    double value = min + RANDOM.nextDouble() * (max - min);
     return Math.min(Math.max(value, min), max - 1);
   }
 
@@ -88,33 +62,8 @@ public final class RandomRangeUtils {
   public static double randomRangeDoubleInclusive(double value1, double value2) {
     double min = Math.min(value1, value2);
     double max = Math.max(value1, value2);
-    double value = min + RandomUtils.nextDouble() * (max - min + 1.0);
+    double value = min + RANDOM.nextDouble() * (max - min + 1.0);
     return Math.min(Math.max(value, min), max);
   }
 
-  /**
-   * Returns a value based on a DoubleRange, including its lower value but not the upper
-   *
-   * @param rangeContainer DoubleRange to check against
-   * @return value
-   */
-  public static double randomDoubleFromDoubleRangeExclusive(DoubleRange rangeContainer) {
-    double min = rangeContainer.getMinimumDouble();
-    double max = rangeContainer.getMaximumDouble();
-    double value = min + RandomUtils.nextDouble() * (max - min);
-    return Math.min(Math.max(value, min), max - 1);
-  }
-
-  /**
-   * Returns a value based on a DoubleRange, including both its lower and upper values
-   *
-   * @param rangeContainer DoubleRange to check against
-   * @return value
-   */
-  public static double randomDoubleFromDoubleRangeInclusive(DoubleRange rangeContainer) {
-    double min = rangeContainer.getMinimumDouble();
-    double max = rangeContainer.getMaximumDouble();
-    double value = min + RandomUtils.nextDouble() * (max - min + 1.0);
-    return Math.min(Math.max(value, min), max);
-  }
 }
