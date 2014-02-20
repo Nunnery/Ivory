619cfd22df2a029a1c3c0f0ecc6dca4d19e62f81
Richard Harrah <rmh4209@gmail.com>
2014-02-20 00:16:21 -0500

    FancyMessage does things on every version now

--- v1_4_R1/src/main/java/mkremins/fanciful/v_1_4_R1/FancyMessage.java
+++ v1_4_R1/src/main/java/mkremins/fanciful/v_1_4_R1/FancyMessage.java
@@ -1,34 +1,22 @@
 package mkremins.fanciful.v_1_4_R1;
 
-import net.minecraft.server.v1_4_R1.NBTTagCompound;
-
 import org.bukkit.ChatColor;
-import org.bukkit.craftbukkit.v1_4_R1.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.json.JSONException;
-import org.json.JSONStringer;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import mkremins.fanciful.IFancyMessage;
 
 public final class FancyMessage implements IFancyMessage {
 
-  private final List<MessagePart> messageParts;
+  private final StringBuilder stringBuilder;
 
   public FancyMessage(final String firstPartText) {
-    messageParts = new ArrayList<MessagePart>();
-    messageParts.add(new MessagePart(firstPartText));
+    stringBuilder = new StringBuilder(firstPartText);
   }
 
   @Override
   public IFancyMessage color(final ChatColor color) {
-    if (!color.isColor()) {
-      throw new IllegalArgumentException(color.name() + " is not a color");
-    }
-    latest().color = color;
+    stringBuilder.append(color);
     return this;
   }
 
@@ -38,102 +26,65 @@ public final class FancyMessage implements IFancyMessage {
       if (!style.isFormat()) {
         throw new IllegalArgumentException(style.name() + " is not a style");
       }
+      stringBuilder.append(style);
     }
-    latest().styles = styles;
     return this;
   }
 
   @Override
   public IFancyMessage file(final String path) {
-    onClick("open_file", path);
     return this;
   }
 
   @Override
   public IFancyMessage link(final String url) {
-    onClick("open_url", url);
     return this;
   }
 
   @Override
   public IFancyMessage suggest(final String command) {
-    onClick("suggest_command", command);
     return this;
   }
 
   @Override
   public IFancyMessage command(final String command) {
-    onClick("run_command", command);
     return this;
   }
 
   @Override
   public IFancyMessage achievementTooltip(final String name) {
-    onHover("show_achievement", "achievement." + name);
     return this;
   }
 
   @Override
   public IFancyMessage itemTooltip(final String itemJSON) {
-    onHover("show_item", itemJSON);
     return this;
   }
 
   @Override
   public IFancyMessage itemTooltip(final ItemStack itemStack) {
-    return itemTooltip(CraftItemStack.asNMSCopy(itemStack).save(new NBTTagCompound()).toString());
+    return this;
   }
 
   @Override
   public IFancyMessage tooltip(final String text) {
-    onHover("show_text", text);
     return this;
   }
 
   @Override
   public IFancyMessage then(final Object obj) {
-    messageParts.add(new MessagePart(obj.toString()));
+    stringBuilder.append(String.valueOf(obj));
     return this;
   }
 
   @Override
   public String toJSONString() {
-    final JSONStringer json = new JSONStringer();
-    try {
-      if (messageParts.size() == 1) {
-        latest().writeJson(json);
-      } else {
-        json.object().key("text").value("").key("extra").array();
-        for (final MessagePart part : messageParts) {
-          part.writeJson(json);
-        }
-        json.endArray().endObject();
-      }
-    } catch (final JSONException e) {
-      throw new RuntimeException("invalid message");
-    }
-    return json.toString();
+    return stringBuilder.toString();
   }
 
   @Override
   public void send(Player player) {
-    // do nothing
-  }
-
-  private MessagePart latest() {
-    return messageParts.get(messageParts.size() - 1);
-  }
-
-  private void onClick(final String name, final String data) {
-    final MessagePart latest = latest();
-    latest.clickActionName = name;
-    latest.clickActionData = data;
-  }
-
-  private void onHover(final String name, final String data) {
-    final MessagePart latest = latest();
-    latest.hoverActionName = name;
-    latest.hoverActionData = data;
+    player.sendMessage(toJSONString());
   }
 
 }
