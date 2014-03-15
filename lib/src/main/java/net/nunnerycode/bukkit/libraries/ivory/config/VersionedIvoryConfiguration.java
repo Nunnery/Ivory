4708ed26a45cb3c75245151c7e459f3e039046b7
Richard Harrah <topplethenunnery@gmail.com>
2014-03-15 00:07:59 -0400

    adding VIC

---
+++ lib/src/main/java/net/nunnerycode/bukkit/libraries/ivory/config/VersionedIvoryConfiguration.java
@@ -0,0 +1,17 @@
+package net.nunnerycode.bukkit.libraries.ivory.config;
+
+public interface VersionedIvoryConfiguration {
+
+  String getVersion();
+
+  String getLocalVersion();
+
+  boolean needsToUpdate();
+
+  boolean update();
+
+  public static enum VersionUpdateType {
+    BACKUP_NO_UPDATE, BACKUP_AND_UPDATE, NOTHING
+  }
+
+}
