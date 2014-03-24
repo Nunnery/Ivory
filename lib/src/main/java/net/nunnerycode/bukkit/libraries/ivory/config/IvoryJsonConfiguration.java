f85bc342a9744c1ab8856833ea249b695082959a
Richard Harrah <topplethenunnery@gmail.com>
2014-03-24 02:10:54 -0400

    adding static method for loading JsonConfiguration

--- lib/src/main/java/net/nunnerycode/bukkit/libraries/ivory/config/IvoryJsonConfiguration.java
+++ lib/src/main/java/net/nunnerycode/bukkit/libraries/ivory/config/IvoryJsonConfiguration.java
@@ -2,10 +2,14 @@ package net.nunnerycode.bukkit.libraries.ivory.config;
 
 import com.dumptruckman.bukkit.configuration.json.JsonConfiguration;
 
+import org.apache.commons.lang.Validate;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.InvalidConfigurationException;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
 
 public class IvoryJsonConfiguration extends JsonConfiguration implements IvoryConfiguration {
 
@@ -13,6 +17,20 @@ public class IvoryJsonConfiguration extends JsonConfiguration implements IvoryCo
 
   public IvoryJsonConfiguration(File file) {
     this.file = file;
+  }
+
+  public static JsonConfiguration loadConfiguration(InputStream stream) {
+    Validate.notNull(stream, "Stream cannot be null");
+
+    JsonConfiguration config = new JsonConfiguration();
+
+    try {
+      config.load(stream);
+    } catch (IOException | InvalidConfigurationException ex) {
+      Bukkit.getLogger().log(Level.SEVERE, "Cannot load configuration from stream", ex);
+    }
+
+    return config;
   }
 
   @Override
