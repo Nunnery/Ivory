3f9a37ca42656e39e8420d46338a1cd452fcafb5
Richard Harrah <topplethenunnery@gmail.com>
2014-03-24 02:01:55 -0400

    json testing

--- plugin/src/main/java/net/nunnerycode/bukkit/ivoryplug/IvoryPlugin.java
+++ plugin/src/main/java/net/nunnerycode/bukkit/ivoryplug/IvoryPlugin.java
@@ -1,10 +1,13 @@
 package net.nunnerycode.bukkit.ivoryplug;
 
+import com.dumptruckman.bukkit.configuration.json.JsonConfiguration;
+
 import net.nunnerycode.java.libraries.cannonball.DebugPrinter;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.util.logging.Level;
 
 public class IvoryPlugin extends JavaPlugin {
@@ -15,7 +18,17 @@ public class IvoryPlugin extends JavaPlugin {
   public void onEnable() {
     debugPrinter = new DebugPrinter(getDataFolder().getPath(), "debug.log");
 
-    debug(Level.INFO, "Running with server package: " + Bukkit.getServer().getClass().getPackage().getName());
+    JsonConfiguration jsonConfiguration = new JsonConfiguration();
+    jsonConfiguration.set("test.field.please.ignore", "okay");
+    jsonConfiguration.set("test.field.please.ignore2", true);
+    try {
+      jsonConfiguration.save(new File(getDataFolder(), "jsonConfig.json"));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    debug(Level.INFO,
+          "Running with server package: " + Bukkit.getServer().getClass().getPackage().getName());
     debug(Level.INFO, "v" + getDescription().getVersion() + " enabled");
   }
 
