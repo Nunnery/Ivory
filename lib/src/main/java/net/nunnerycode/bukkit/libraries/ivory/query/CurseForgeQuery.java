8626e5fb5ec5812ca18a09c43bdfdfcb6f9f556c
Richard Harrah <topplethenunnery@gmail.com>
2014-03-30 14:49:28 -0400

    adding javadoc to CurseForgeQuery

--- lib/src/main/java/net/nunnerycode/bukkit/libraries/ivory/query/CurseForgeQuery.java
+++ lib/src/main/java/net/nunnerycode/bukkit/libraries/ivory/query/CurseForgeQuery.java
@@ -11,6 +11,9 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
+/**
+ * A class that is used for querying the CurseForge API for BukkitDev.
+ */
 public final class CurseForgeQuery {
 
   private static final String API_QUERY = "/servermods/projects?search=";
@@ -20,6 +23,13 @@ public final class CurseForgeQuery {
   private static final String PROJECT_SLUG = "slug";
   private static final String PROJECT_STAGE = "stage";
 
+  /**
+   * Returns a {@link net.nunnerycode.bukkit.libraries.ivory.query.CurseForgeQuery.BukkitProject}
+   * with the given name from BukkitDev.
+   *
+   * @param name name of the project
+   * @return project if it exists, null if it doesn't
+   */
   public BukkitProject query(String name) {
     URL url;
     URLConnection conn;
@@ -58,10 +68,17 @@ public final class CurseForgeQuery {
     return null;
   }
 
+  /**
+   * An enum representing the various stages a project may have on BukkitDev.
+   */
   public static enum ProjectStage {
     PLANNING, ALPHA, BETA, RELEASE, DELETED, MATURE, INACTIVE, ABANDONED
   }
 
+  /**
+   * A simple class to hold data about a project on BukkitDev. </p> Creating a new BukkitProject
+   * will not create a new project on BukkitDev.
+   */
   public static class BukkitProject {
 
     private long id;
@@ -69,6 +86,14 @@ public final class CurseForgeQuery {
     private String slug;
     private ProjectStage projectStage;
 
+    /**
+     * Instantiates a new BukkitProject.
+     *
+     * @param id           id of the project
+     * @param name         name of the project
+     * @param slug         slug of the project
+     * @param projectStage stage of the project
+     */
     public BukkitProject(long id, String name, String slug,
                          ProjectStage projectStage) {
       this.id = id;
@@ -77,18 +102,38 @@ public final class CurseForgeQuery {
       this.projectStage = projectStage;
     }
 
+    /**
+     * Gets and returns the project's ID.
+     *
+     * @return id of the project
+     */
     public long getId() {
       return id;
     }
 
+    /**
+     * Gets and returns the project's name.
+     *
+     * @return name of the project
+     */
     public String getName() {
       return name;
     }
 
+    /**
+     * Gets and returns the project's slug.
+     *
+     * @return slug of the project
+     */
     public String getSlug() {
       return slug;
     }
 
+    /**
+     * Gets and returns the project's {@link net.nunnerycode.bukkit.libraries.ivory.query.CurseForgeQuery.ProjectStage}.
+     *
+     * @return stage of the project
+     */
     public ProjectStage getProjectStage() {
       return projectStage;
     }
