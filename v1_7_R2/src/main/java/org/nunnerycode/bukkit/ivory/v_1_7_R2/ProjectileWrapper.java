13d559c99b4dabf5af75d6c5bd9fa96f1ce2147e
Richard Harrah <topplethenunnery@gmail.com>
2014-04-02 21:47:24 -0400

    adding ProjectileWrapper

---
+++ v1_7_R2/src/main/java/org/nunnerycode/bukkit/ivory/v_1_7_R2/ProjectileWrapper.java
@@ -0,0 +1,22 @@
+package org.nunnerycode.bukkit.ivory.v_1_7_R2;
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Projectile;
+import org.bukkit.projectiles.ProjectileSource;
+import org.nunnerycode.bukkit.ivory.IProjectileWrapper;
+
+public final class ProjectileWrapper implements IProjectileWrapper {
+
+  @Override
+  public LivingEntity getShooter(Projectile projectile) {
+    if (projectile == null) {
+      return null;
+    }
+    ProjectileSource projectileSource = projectile.getShooter();
+    if (!(projectileSource instanceof LivingEntity)) {
+      return null;
+    }
+    return (LivingEntity) projectileSource;
+  }
+
+}
