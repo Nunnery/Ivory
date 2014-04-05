b9403445260297e9cc016222c8b55a01a7ba7cad
Richard Harrah <topplethenunnery@gmail.com>
2014-04-05 02:21:30 -0400

    adding LocationUtils class

---
+++ lib/src/main/java/net/nunnerycode/bukkit/libraries/ivory/utils/LocationUtils.java
@@ -0,0 +1,38 @@
+package net.nunnerycode.bukkit.libraries.ivory.utils;
+
+import org.bukkit.Location;
+
+public final class LocationUtils {
+
+  private LocationUtils() {
+    // do nothing
+  }
+  
+  public static boolean isLocationBetween(Location loc1, Location loc2, Location locToTest) {
+    if (loc1 == null || loc2 == null || locToTest == null) {
+      throw new IllegalArgumentException("Location cannot be null");
+    }
+
+    int minX = Math.min(loc1.getBlockX(), loc2.getBlockX());
+    int maxX = Math.min(loc1.getBlockX(), loc2.getBlockX());
+    int minY = Math.min(loc1.getBlockY(), loc2.getBlockY());
+    int maxY = Math.min(loc1.getBlockY(), loc2.getBlockY());
+    int minZ = Math.min(loc1.getBlockZ(), loc2.getBlockZ());
+    int maxZ = Math.min(loc1.getBlockZ(), loc2.getBlockZ());
+
+    int x = locToTest.getBlockX();
+    int y = locToTest.getBlockY();
+    int z = locToTest.getBlockZ();
+
+    return minX <= x && x <= maxX && minY <= y && y <= maxY && minZ <= z && z <= maxZ;
+  }
+
+  public static boolean isLocationDistanceFromLocation(Location loc1, Location loc2, double dist) {
+    if (loc1 == null || loc2 == null) {
+      throw new IllegalArgumentException("Location cannot be null");
+    }
+    double distance = loc1.distanceSquared(loc2);
+    return distance >= (dist * dist);
+  }
+
+}
